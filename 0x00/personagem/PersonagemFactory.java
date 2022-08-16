afc530f7987544838816c85cb614b5d2cfb8b8cc
Thiago Sougo <61519490+thiagosougo@users.noreply.github.com>
2022-08-16 20:36:24 -0300

    Ex

--- 0x00/personagem/PersonagemFactory.java
+++ 0x00/personagem/PersonagemFactory.java
@@ -1,4 +1,3 @@
 public abstract class PersonagemFactory {
-    public abstract Personagem createPersonagem(TipoPersonagem tipo, String nome);
+    public abstract Personagem createPersonagem(TipoPersonagem tipoPersonagem, String nome);
 }
-
