c789ccd07e9ed5f4dd6fc3fce9dd607349223730
Thiago Sougo <thiagosougo@gmail.com>
2022-08-16 20:11:12 -0300

    Ex 0

---
+++ personagem/src/WesterosPersonagemFactory.java
@@ -0,0 +1,14 @@
+public class WesterosPersonagemFactory extends PersonagemFactory{
+    @Override
+    public Personagem createPersonagem(TipoPersonagem tipoPersonagem, String nome) {
+        Personagem personagem;
+        if (tipoPersonagem.equals(TipoPersonagem.MAGO)) {
+            personagem = new Mago(nome, 8, 3, 3, 3 ,4);
+        } else if (tipoPersonagem.equals(TipoPersonagem.GUERREIRO)) {
+            personagem = new Guerreiro(nome,1, 8, 7, 7, 10);
+        } else {
+            personagem = new Ladrao(nome,  0, 9, 7, 10, 7);
+        }
+        return personagem;
+    }
+}
