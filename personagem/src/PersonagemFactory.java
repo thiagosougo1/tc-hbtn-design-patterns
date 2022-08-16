d9dbe3ac83e70e95464dcb38d28148e6b5093400
Thiago Sougo <61519490+thiagosougo@users.noreply.github.com>
2022-08-16 20:27:16 -0300

    Ex 0

--- personagem/src/PersonagemFactory.java
+++
@@ -1,4 +0,0 @@
-public abstract class PersonagemFactory {
-    public abstract Personagem createPersonagem(TipoPersonagem tipo, String nome);
-}
-
--- personagem/src/TipoPersonagem.java
+++
@@ -1,3 +0,0 @@
-public enum TipoPersonagem {
-    GUERREIRO, MAGO, LADRAO
-}
