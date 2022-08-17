725970f03ad97db169f45063915f9f7b7615c1e6
Thiago Sougo <61519490+thiagosougo@users.noreply.github.com>
2022-08-16 21:17:40 -0300

    Ex 0

--- 0x00/personagem/Guerreiro.java
+++ 0x00/personagem/Guerreiro.java
@@ -1,19 +1,14 @@
-public class Guerreiro extends Personagem {
+public class Guerreiro extends Personagem{
 
-    public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+    public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) throws Exception {
         super(nome, TipoPersonagem.GUERREIRO, inteligencia, forca, vigor, resistencia, destreza);
-        if (inteligencia > forca && destreza > forca){
-            throw new IllegalArgumentException("Atributos invalidos para Guerreiro");
+        if(forca<inteligencia || forca<destreza) {
+            throw new Exception("Atributos invalidos para GUERREIRO");
         }
     }
 
     @Override
     public double getDanoAtaque() {
         return (forca * 0.8) + (vigor * 0.05) + (destreza * 0.1) + (inteligencia * 0.05);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
     }
 }
--- 0x00/personagem/Ladrao.java
+++ 0x00/personagem/Ladrao.java
@@ -1,10 +1,14 @@
-public class Ladrao extends Personagem {
-    public Ladrao(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+public class Ladrao extends Personagem{
+
+    public Ladrao(String nome,  int inteligencia, int forca, int vigor, int resistencia, int destreza) throws Exception {
         super(nome, TipoPersonagem.LADRAO, inteligencia, forca, vigor, resistencia, destreza);
+        if (destreza < forca || destreza < inteligencia) {
+            throw new Exception("Atributos invalidos para LADRAO");
+        }
     }
 
     @Override
     public double getDanoAtaque() {
-        return (forca * 0.5) + (destreza * 0.35) + (vigor * 0.1) + (inteligencia* 0.05);
+        return (forca * 0.5) + (destreza * 0.35) + (vigor * 0.1) + (inteligencia * 0.05);
     }
 }
