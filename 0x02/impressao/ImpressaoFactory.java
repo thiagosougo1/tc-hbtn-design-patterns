748ffadf28081656012c2b67153b62a5b10dd728
Thiago Sougo <61519490+thiagosougo@users.noreply.github.com>
2022-08-17 19:20:03 -0300

    Ex 0

--- 0x02/impressao/ImpressaoFactory.java
+++ 0x02/impressao/ImpressaoFactory.java
@@ -1,10 +1,22 @@
 package impressao;
 
-import javax.imageio.ImageReadParam;
 
 public class ImpressaoFactory {
-    public static Impressao createImpressao(TamanhoImpressao tamanho, int totalPaginas, int quantidadesColoridas, boolean ehFrenteVerso){
-        return null;
-    }
 
+    public static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int totalPaginas,  int quantPaginasColoridas, boolean ehFrenteVerso) {
+        if (tamanhoImpressao == TamanhoImpressao.A2) {
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.28,
+                    0.18, 0.32, 0.22);
+            return impressao;
+        }  else if (tamanhoImpressao == TamanhoImpressao.A3) {
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.25,
+                    0.15, 0.30, 0.20);
+            return impressao;
+        } else {
+            Impressao impressao = new Impressao(totalPaginas, quantPaginasColoridas, ehFrenteVerso, 0.20,
+                    0.10, 0.25, 0.15);
+            return impressao;
+        }
+
+    }
 }
